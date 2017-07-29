8140315f29424f08314c55d0db12f607d10638a0
wuzhao <liwuzhao97@gmail.com>
2017-07-30 03:42:18 +0800

    Use SSL and the Puma webserver in production

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   get  '/home', to: 'static_pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
   root 'static_pages#home'
 end
